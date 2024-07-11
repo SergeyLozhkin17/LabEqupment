505ccfa5926aab2549fe904e711802a710f89c1f
noman17rus <noman171991@gmail.com>
2024-07-11 14:43:37 +0400

    change keyboard behaviour

--- app/src/main/java/com/example/labequpment/ui/screens/add_eqipment_screen/AddEquipmentScreen.kt
+++ app/src/main/java/com/example/labequpment/ui/screens/add_eqipment_screen/AddEquipmentScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -21,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -68,6 +70,7 @@ private fun AddEquipmentScreenBody(
                 onValueChange = { onItemValueChange(equipmentDetails.copy(name = it)) },
                 label = { Text(text = "Название прибора") },
                 shape = MaterialTheme.shapes.medium,
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
                 modifier = modifier.fillMaxWidth()
             )
             OutlinedTextField(
@@ -75,6 +78,7 @@ private fun AddEquipmentScreenBody(
                 onValueChange = { onItemValueChange(equipmentDetails.copy(factoryNumber = it)) },
                 label = { Text(text = "Заводской №") },
                 shape = MaterialTheme.shapes.medium,
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
                 modifier = modifier.fillMaxWidth()
             )
             VerificationDatePicker(
@@ -106,7 +110,8 @@ private fun AddEquipmentScreenBody(
                     },
                     modifier = modifier
                         .weight(1f)
-                        .height(48.dp).fillMaxWidth()
+                        .height(48.dp)
+                        .fillMaxWidth()
                 ) {
                     Text(text = "Сохранить")
                 }
