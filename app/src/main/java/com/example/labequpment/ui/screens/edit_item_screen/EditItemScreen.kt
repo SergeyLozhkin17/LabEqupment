fa429be5b5f1641f728df3b79759b22f31fbcccf
noman17 <noman171991@gmail.com>
2024-07-16 13:34:06 +0400

    add screen and ViewModel for edit

---
+++ app/src/main/java/com/example/labequpment/ui/screens/edit_item_screen/EditItemScreen.kt
@@ -0,0 +1,8 @@
+package com.example.labequpment.ui.screens.edit_item_screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EditItemScreen() {
+
+}
---
+++ app/src/main/java/com/example/labequpment/ui/screens/edit_item_screen/EditItemScreenViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.labequpment.ui.screens.edit_item_screen
+
+import androidx.lifecycle.ViewModel
+
+class EditItemScreenViewModel : ViewModel() {
+}
